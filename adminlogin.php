485e9c92b80dfa2d3fce167a754c97e244548d58
ComfyNeko <80762247+ComfyNeko@users.noreply.github.com>
2021-03-19 06:48:42 -0500

    Update adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -1,47 +1,5 @@
 <?php
-    $url='localhost';
-    $username='root';
-    $password='';
-    $conn=mysqli_connect($url,$username,$password,"quizdb");
-    if(!$conn){
-        die('Could not Connect My Sql:' .mysql_error());
-    }
-
-  if(isset($_POST["submit"])){  
-  
-if(!empty($_POST['email']) && !empty($_POST['password'])) {  
-    $user=$_POST['email'];  
-    $pass=$_POST['password'];  
-     $pass=md5($pass);
-    $con=mysql_connect('localhost','root','') or die(mysql_error());  
-    mysql_select_db('quizdb') or die("cannot select DB");  
-  
-    $query=mysql_query("SELECT * FROM admin WHERE email='".$user."' AND password='".$pass."'");  
-    $numrows=mysql_num_rows($query);  
-    if($numrows!=0)  
-    {  
-    while($row=mysql_fetch_assoc($query))  
-    {  
-    $dbusername=$row['email'];  
-    $dbpassword=$row['password'];  
-    }  
-  
-    if($user == $dbusername && $pass == $dbpassword)  
-    {  
-    session_start();  
-    $_SESSION['sess_user']=$user;  
-  
-    /* Redirect browser */  
-    header("Location: quizzie.php");  
-    }  
-    } else {  
-    echo "Invalid username or password!";  
-    }  
-  
-} else {  
-    echo "All fields are required!";  
-}  
-}  
+ echo ("hello");  
 ?>  
 
 
